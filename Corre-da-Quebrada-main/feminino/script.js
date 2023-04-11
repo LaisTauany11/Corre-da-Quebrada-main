795d28a62ba81931ff83334b04dcaf289b7a26e9
LaisTauany11 <90428706+LaisTauany11@users.noreply.github.com>
2023-04-10 23:56:03 -0300

    pronto

---
+++ Corre-da-Quebrada-main/feminino/script.js
@@ -0,0 +1,97 @@
+let nike = document.getElementById("nike");
+let adidas = document.getElementById("adidas");
+let puma = document.getElementById("puma");
+let vans = document.getElementById("vans");
+let oakley = document.getElementById("oakley");
+
+let nikeT = document.getElementById("nikeT");
+let adidasT = document.getElementById("adidasT");
+let pumaT = document.getElementById("pumaT");
+let vansT = document.getElementById("vansT");
+let oakleyT = document.getElementById("oakleyT");
+
+function mostraTodos(){
+   
+    nike.style.display = 'flex';
+    adidas.style.display = 'flex';
+    puma.style.display = 'flex';
+    vans.style.display = 'flex';
+    oakley.style.display = 'flex';
+
+    adidasT.style.display = 'flex';
+    nikeT.style.display = 'flex';
+    pumaT.style.display = 'flex';
+    vansT.style.display = 'flex';
+    oakleyT.style.display = 'flex';
+}
+
+
+function mostraNike(){
+    nike.style.display = 'flex';
+    adidas.style.display = 'none';
+    puma.style.display = 'none';
+    vans.style.display = 'none';
+    oakley.style.display = 'none';
+
+    adidasT.style.display = 'none';
+    pumaT.style.display = 'none';
+    vansT.style.display = 'none';
+    oakleyT.style.display = 'none';
+    nikeT.style.display = 'flex';   
+}
+
+function mostraAdidas(){
+    adidas.style.display = 'flex';
+    nike.style.display = 'none';
+    puma.style.display = 'none';
+    vans.style.display = 'none';
+    oakley.style.display = 'none';
+
+    nikeT.style.display = 'none';
+    pumaT.style.display = 'none';
+    vansT.style.display = 'none';
+    adidasT.style.display = 'flex';
+    oakleyT.style.display = 'none';
+}
+
+function mostraPuma(){
+    puma.style.display = 'flex';
+    nike.style.display = 'none';
+    vans.style.display = 'none';
+    oakley.style.display = 'none';
+    adidas.style.display = 'none';
+    
+    adidasT.style.display = 'none';
+    nikeT.style.display = 'none';
+    vansT.style.display = 'none';
+    oakleyT.style.display = 'none';
+    pumaT.style.display = 'flex';
+}
+
+function mostraVans(){
+    puma.style.display = 'none';
+    nike.style.display = 'none';
+    oakley.style.display = 'none';
+    vans.style.display = 'flex';
+    adidas.style.display = 'none';
+    
+    adidasT.style.display = 'none';
+    nikeT.style.display = 'none';
+    pumaT.style.display = 'none';
+    oakleyT.style.display = 'none';
+    vansT.style.display = 'flex';
+}
+
+function mostraOakley(){
+    oakley.style.display = 'flex';
+    vans.style.display = 'none';
+    puma.style.display = 'none';
+    nike.style.display = 'none';
+    adidas.style.display = 'none';
+    
+    adidasT.style.display = 'none';
+    nikeT.style.display = 'none';
+    pumaT.style.display = 'none';
+    vansT.style.display = 'none';
+    oakleyT.style.display = 'flex';
+}
